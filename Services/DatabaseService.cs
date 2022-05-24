13b1a7f298eb0000e0d1d97178a5360bc63f814c
CryptoWingnut <cryptowingnut@gmail.com>
2022-05-24 14:41:46 -0300

    Re-enable mainnet

--- Services/DatabaseService.cs
+++ Services/DatabaseService.cs
@@ -30,7 +30,7 @@ public class DatabaseService {
         _logger = logger;
 
         _details = new List<OblivionDetails> {
-            //new() { ChainID = ChainID.BSC_Mainnet, ReleaseStartingBlock = 16636640 },
+            new() { ChainID = ChainID.BSC_Mainnet, ReleaseStartingBlock = 16636640 },
             new() { ChainID = ChainID.BSC_Testnet, ReleaseStartingBlock = 17931172 },
             new() { ChainID = ChainID.Nervos_Testnet }
         };
