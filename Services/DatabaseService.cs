c0aa15a8be9b6ab589a77e196db6e9c3201df6f0
CryptoWingnut <cryptowingnut@gmail.com>
2022-06-01 19:30:10 -0300

    Added Nervos testnet minting contract details

--- Services/DatabaseService.cs
+++ Services/DatabaseService.cs
@@ -34,7 +34,7 @@ public class DatabaseService {
         _details = new List<OblivionDetails> {
             new() { ChainID = ChainID.BSC_Mainnet, ReleaseStartingBlock = 16636640 },
             new() { ChainID = ChainID.BSC_Testnet, ReleaseStartingBlock = 17931172 },
-            new() { ChainID = ChainID.Nervos_Testnet }
+            new() { ChainID = ChainID.Nervos_Testnet, ReleaseStartingBlock = 208227 }
         };
     }
 
