3dfddcc4b96d49c556fde45bdc1df26ba66585c5
nnamss <10407499+nnamss@users.noreply.github.com>
2022-12-01 13:14:42 -0500

    remove accidently pushed func

--- Objects/FactoryNftList.cs
+++ Objects/FactoryNftList.cs
@@ -4,8 +4,4 @@
 public class FactoryNftList {
     public string Contract { get; set; }
     public List<FactoryNftDetails> Nfts { get; set; } = new();
-
-    public bool is1155(ChainID chainId) {
-        return Contract == Contracts.Nft1155Factories[chainId];
-    }
 }
